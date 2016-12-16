885a0af31520dca808563f93e00b8a9206d33d01
Dan Bikle <bikle@bikle.com>
2016-12-16 15:54:39 -0800

    more

--- flask12.py
+++ flask12.py
@@ -27,6 +27,12 @@ def home():
 def favicon():
     return send_from_directory(os.path.join(application.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+from flask import render_template
+@application.route("/template10/")
+def template10():
+    return render_template('template10.html')
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     application.run(host='0.0.0.0', port=port)
