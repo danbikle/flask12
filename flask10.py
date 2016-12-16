396a35a0fba07aa4ff4d453de718f974f75c878f
Dan Bikle <bikle@bikle.com>
2016-12-16 03:14:26 -0800

    more

--- flask10.py
+++ flask10.py
@@ -16,9 +16,19 @@
 # import pdb
 import os
 from   flask import Flask
+from   flask import send_from_directory
 application = Flask(__name__)
-                               
+            
+@application.route("/")
+def home():
+    return send_from_directory(os.path.join(application.root_path, 'static'),'home.html')
+
+@application.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(application.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     application.run(host='0.0.0.0', port=port)
 'bye'
+
